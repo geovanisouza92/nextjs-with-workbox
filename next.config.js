f213ba08973045cdafd98a520a471d42fb61d58f
Geovani de Souza <geovanisouza92@gmail.com>
2022-01-26 16:01:23 -0300

    adding workbox

--- next.config.js
+++ next.config.js
@@ -1,3 +1,20 @@
+const { GenerateSW } = require('workbox-webpack-plugin');
+
+/** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  webpack(/** @type {import('webpack').Configuration} */ config, options) {
+    if (options.isServer || options.isDev) {
+      return config;
+    }
+
+    config.plugins.push(
+      new GenerateSW({
+        swDest: 'static/service-worker.js',
+        include: [/.*static.*/i]
+      })
+    );
+
+    return config;
+  },
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,25 @@
+import { useEffect } from 'react'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', function () {
+        navigator.serviceWorker.register('/_next/static/service-worker.js').then(
+          function (registration) {
+            console.log(
+              'Service Worker registration successful with scope: ',
+              registration.scope
+            )
+          },
+          function (err) {
+            console.log('Service Worker registration failed: ', err)
+          }
+        )
+      })
+    }
+  }, [])
+
   return <Component {...pageProps} />
 }
 
